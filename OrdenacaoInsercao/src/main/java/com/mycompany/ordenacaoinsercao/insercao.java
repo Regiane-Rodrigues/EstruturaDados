990dae710fd2837d0aac3f6041638a4845bfab23
Regiane Rodrigues <107564169+Regiane-Rodrigues@users.noreply.github.com>
2023-06-06 22:06:00 -0300

    Ordenacao Por Selação

---
+++ OrdenacaoInsercao/src/main/java/com/mycompany/ordenacaoinsercao/insercao.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ordenacaoinsercao;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author aluno
+ */
+public class insercao {
+
+    public static void Insercao() {
+        int tamanhoVetor = Integer.parseInt(JOptionPane.showInputDialog("Informe o tamanho do vetor"));
+
+        int[] vetor = new int[tamanhoVetor];
+        
+        for(int i = 0; i < vetor.length-1; i++){
+        
+        vetor[i] = 
+        
+        
+        }
+
+    }
+
+}
